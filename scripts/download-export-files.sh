a67ed2842c442d27e3fee2937d8731fe7b3a7a09
Abbe98 <albin.post@gmail.com>
2023-05-28 20:39:14 +0200

    update export API endpoint

--- scripts/download-export-files.sh
+++ scripts/download-export-files.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 declare -A files=(
-    ["lamnings.jsonld"]="https://fornpunkt.se/apis/lamnings?format=jsonld&scope=all"
-    ["tags.jsonld"]="https://fornpunkt.se/apis/tags?format=jsonld"
-    ["comments.jsonld"]="https://fornpunkt.se/apis/comments?format=jsonld"
-    ["wikipedia-annotations.jsonld"]="https://fornpunkt.se/apis/wikipedia-annotations?format=jsonld"
-    ["generic-annotations.jsonld"]="https://fornpunkt.se/apis/generic-annotations?format=jsonld"
+    ["lamnings.jsonld"]="https://fornpunkt.se/apis/export/lamnings?format=jsonld&scope=all"
+    ["tags.jsonld"]="https://fornpunkt.se/apis/export/tags?format=jsonld"
+    ["comments.jsonld"]="https://fornpunkt.se/apis/export/comments?format=jsonld"
+    ["wikipedia-annotations.jsonld"]="https://fornpunkt.se/apis/export/wikipedia-annotations?format=jsonld"
+    ["generic-annotations.jsonld"]="https://fornpunkt.se/apis/export/generic-annotations?format=jsonld"
     ["observation-types.jsonld"]="https://fornpunkt.se/observationstyper.jsonld"
 )
 
